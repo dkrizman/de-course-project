d560428af93be0d3184e5b084288256105a8aab4
Daniel Krizman <danielkrizman@Daniels-MacBook-Air.local>
2026-08-24 19:00:44 +0300

    final-project-stage1 - #3 - better prints in main

--- src/pipeline/__main__.py
+++ src/pipeline/__main__.py
@@ -12,9 +12,7 @@ from pipeline.gold_layer_processing.inspect_gold import inspect_gold
 
 def main():
     """Main entry point"""
-    # print("Starting trip data ingestion...")
-    
-    # Run migrations first
+
     print("Running database migrations...")
     run_migrations()
 
@@ -30,25 +28,31 @@ def main():
     try:
         if layer == "ingest-to-bronze":
             ingest_to_bronze(region, window)
+            print("✓ Ingestion complete")
         elif layer == "transform-to-silver":
             with psycopg.connect(database_url()) as conn:
                 transform_to_silver(conn, region, window)
+            print("✓ transform_to_silver complete")
         elif layer == "bronze":
             inspect_bronze(region, window)
+            print("✓ inspect_bronze complete")
         elif layer == "silver":
             inspect_silver(region, window)
+            print("✓ inspect_silver complete")
         elif layer == "transform-to-gold":
             with psycopg.connect(database_url()) as conn:
                 transform_to_gold(conn, window)
+            print("✓ transform_to_gold complete")
         elif layer == "daily-station-trips":
             with psycopg.connect(database_url()) as conn:
                 inspect_gold(conn, window, station, region)
+            print("✓ inspect_gold complete")
         else:
             raise ValueError(f"Unknown layer: {layer}")
     except Exception as e:
         print(f"✗ Ingestion failed: {e}")
         raise
-    print("✓ Ingestion complete")
+    
 
 if __name__ == "__main__":
     main()
